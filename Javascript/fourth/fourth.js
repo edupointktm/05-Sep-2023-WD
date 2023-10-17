b80ebf62e6391ca245acdc67edba7c17de86099a
edupointktm <edupointktm@gmail.com>
2023-10-17 21:41:42 +0545

    operator

---
+++ Javascript/fourth/fourth.js
@@ -0,0 +1,99 @@
+// conditional operator or comperision
+// < <= > >= == === 
+// it return the result as True or Flase
+
+// console.log(15>5) //true
+// console.log(5<5)  //False
+// console.log(5<=5) //True
+// console.log(5=="5")  //True
+// console.log(5==="5")  //False
+
+//<<<<<<<<<< Logic Operator >>>>>>>>>>>>>>>>
+// AND => && , OR => || , NOT => !
+// it is used to combine conditional operator
+
+// console.log(5>5  && 5==5  && 5<=5)
+
+// console.log(5>5 || 5==5 || 5>5)  //
+
+// console.log(15!=5)
+
+
+// <<<<<<<<<<<<<< If statement >>>>>>>>>>>>>>>
+// 1. if
+// 2. if else
+// 3. if else if
+// 4. nested if else
+
+// if(5==15){
+//     console.log("Welcome")
+// }
+// else{
+//     console.log("Tinkune, Kathmandu")
+// }
+
+// if(cond1){
+
+
+// }
+// else if(cond2){
+
+
+// }
+// else if (cond3){
+
+
+// }
+// else if(condn){
+
+
+// }
+// else{
+
+
+// }
+
+
+//WAP to ask from user enter week day number and display week name as given by user
+
+// var wn =3
+
+// if(wn==1){
+//     console.log("Sunday")
+// }
+// else if(wn ==2){
+//     console.log("Monday")
+// }
+// else if(wn == 3){
+//     console.log("Tuesday")
+// }
+// else if (wn == 4){
+//     console.log("Wednesday")
+// }
+// else if(wn == 5){
+//     console.log("Thursday")
+// }
+// else if(wn == 6){
+//     console.log("Friday")
+// }
+// else if(wn == 7){
+//     console.log("Saturday")
+// }
+// else{
+//     console.log("Sorry! Enter number is not week day number")
+// }
+
+// WAP to ask any one number from user and check given number is ODD or EVEN
+
+// 8=> even
+
+let num = 8
+
+let rem = num % 2
+
+if(rem==0){
+    EVEN
+}
+else{
+    ODD
+}
